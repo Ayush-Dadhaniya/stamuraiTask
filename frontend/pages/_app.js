127f74ce4816c5ea7edb8587f098157a84512c93
Ayush-Dadhaniya <ayushdadhaniya05@gmail.com>
2025-05-03 14:10:15 +0530

    Added Tab logo

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <link rel="icon" href="/task_management_logo.svg" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }

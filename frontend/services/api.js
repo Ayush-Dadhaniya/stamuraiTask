86825c41667fabd70c8ee441aa4ae45b5a801821
Ayush Dadhaniya <137676958+Ayush-Dadhaniya@users.noreply.github.com>
2025-06-27 12:08:14 +0530

    Update api.js

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://stamuraitask-production.up.railway.app', // Adjust if your backend runs elsewhere
+  baseURL: 'https://stamurai-task-backend.vercel.app', // Adjust if your backend runs elsewhere
 });
 
 export default api;
